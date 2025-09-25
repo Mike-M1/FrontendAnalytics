7460d0e95dc81bf5558b2a4a711a2e22d8b9ff4d
Mikal Mckeever <mikalmckeever6@gmail.com>
2025-09-25 16:48:39 -0500

    google analytics

--- app/page.js
+++ app/page.js
@@ -1,18 +1,10 @@
 "use client"
 import Image from "next/image";
+
 import { useState, useEffect } from "react";
 
 export default function Home() {
 
-  useEffect(() => {
-    console.log("navigator", navigator);
-    console.log("screen", screen);
-    console.log("document", document);
-    console.log("window", window);
-    console.log("location", location);
-    console.log("history", history);
-    console.log("performance", performance);
-  }, []);
 
   return (
     <div>
